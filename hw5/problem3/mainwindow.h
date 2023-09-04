35fc53e5cf5edb0c0c69b464a4c1e3c46ac26053
gtxzsxxk <2524395907@qq.com>
2023-09-04 23:54:48 +0800

    feat: finish hw5.3

--- hw5/problem3/mainwindow.h
+++ hw5/problem3/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <vector>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -10,12 +12,20 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+    QTimer *scanner;
+    QString image_path;
+    std::vector<std::pair<QString,QString>> images;
+    int image_ptr;
+    void lookup_images();
+    QSize optimize_size(QSize frame, QSize image) const;
+private slots:
+    void next_image();
+    void timer_timeout();
 };
 #endif // MAINWINDOW_H
