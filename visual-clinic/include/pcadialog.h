5658ea6bd6c480cf929457fec02ae3e78360e586
gtxzsxxk <2524395907@qq.com>
2023-09-12 21:17:49 +0800

    feat: add 2d scatter code

--- visual-clinic/include/pcadialog.h
+++ visual-clinic/include/pcadialog.h
@@ -5,27 +5,38 @@
 #include <QTableWidget>
 #include <vector>
 #include <QWidget>
+#include <QChartView>
 #include <QtDataVisualization/Q3DScatter>
 
 namespace Ui {
-class PCADialog;
+    class PCADialog;
 }
 
-class PCADialog : public QDialog
-{
-    Q_OBJECT
+class PCADialog : public QDialog {
+Q_OBJECT
 
 public:
-    explicit PCADialog(QWidget *parent,QTableWidget *tableWidget);
+    explicit PCADialog(QWidget *parent, QTableWidget *tableWidget);
+
     ~PCADialog();
 
 private:
     Ui::PCADialog *ui;
     QTableWidget *tableWidget;
-    QWidget *chartWidget;
+    QWidget *scatter_3d_widget = nullptr;
+    QChartView *scatter_2d_widget = nullptr;
     std::vector<std::vector<float>> points;
     std::vector<bool> point_status;
+
     void go_PCA();
+
+    void init_3d_scatter();
+
+    void init_2d_scatter();
+
+private slots:
+
+    void dimValueChanged(int value);
 };
 
 #endif // PCADIALOG_H
--- visual-clinic/src/pcadialog.cpp
+++ visual-clinic/src/pcadialog.cpp
@@ -3,9 +3,10 @@
 #include "../ui_pcadialog.h"
 
 #include <set>
-#include <QChartView>
 #include <QtDataVisualization/QScatter3DSeries>
 #include <QtDataVisualization/QScatterDataArray>
+#include <QScatterSeries>
+#include <QValueAxis>
 
 #define MAX_MIN_SET(id) do { \
     if(v_##id>v_##id##_max){\
@@ -24,6 +25,8 @@ PCADialog::PCADialog(QWidget *parent, QTableWidget *tableWidget) :
         QDialog(parent),
         ui(new Ui::PCADialog), tableWidget(tableWidget) {
     ui->setupUi(this);
+    connect(ui->spinBox,SIGNAL(valueChanged(int)),this,SLOT(dimValueChanged(int)));
+
     setAttribute(Qt::WA_DeleteOnClose);
     std::set<int> columns;
     for (const auto &item: tableWidget->selectedItems()) {
@@ -72,59 +75,130 @@ void PCADialog::go_PCA() {
     int dim = ui->spinBox->value();
     /* TODO: manage the memory use */
     if (dim == 3) {
-        auto *q3DScatter = new Q3DScatter();
-        q3DScatter->activeTheme()->setType(Q3DTheme::ThemePrimaryColors);
-        q3DScatter->setShadowQuality(QAbstract3DGraph::ShadowQualitySoftLow);
-        q3DScatter->scene()->activeCamera()->setCameraPreset(Q3DCamera::CameraPresetFront);
-        chartWidget = QWidget::createWindowContainer(q3DScatter);
-        q3DScatter->setFlags(q3DScatter->flags() ^ Qt::FramelessWindowHint);
-//        chartWidget = new QWidget(this);
-        chartWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-        /* todo: check here */
-        ui->horizontalLayout->addWidget(chartWidget);
-        ui->horizontalLayout->setStretch(0, 1);
-        ui->horizontalLayout->setStretch(1, 3);
-        auto *b_series = new QScatter3DSeries;
-        auto *m_series = new QScatter3DSeries;
-        QScatterDataArray b_data;
-        QScatterDataArray m_data;
-        auto res = pca(points, 3);
-        float v_0_max = -1000, v_1_max = -1000, v_2_max = -1000;
-        float v_0_min = 1000, v_1_min = 1000, v_2_min = 1000;
-        for (int i = 0; i < points.size(); i++) {
-            float v_0 = res(i, 0);
-            float v_1 = res(i, 1);
-            float v_2 = res(i, 2);
-            if (point_status[i]) {
-                m_data << QVector3D(v_0, v_1, v_2);
-            } else {
-                b_data << QVector3D(v_0, v_1, v_2);
-            }
-            MAX_MIN_SET(0);
-            MAX_MIN_SET(1);
-            MAX_MIN_SET(2);
-        }
-        float v_0_offset, v_1_offset, v_2_offset;
-        OFFSET(0);
-        OFFSET(1);
-        OFFSET(2);
-        q3DScatter->axisX()->setTitle("PC1");
-        q3DScatter->axisY()->setTitle("PC2");
-        q3DScatter->axisZ()->setTitle("PC3");
-        q3DScatter->axisX()->setRange(v_0_min - v_0_offset, v_0_max + v_0_offset);
-        q3DScatter->axisY()->setRange(v_1_min - v_1_offset, v_1_max + v_1_offset);
-        q3DScatter->axisZ()->setRange(v_2_min - v_2_offset, v_2_max + v_2_offset);
-        b_series->dataProxy()->addItems(b_data);
-        b_series->setName("良性肿瘤");
-        b_series->setItemSize(0.1f);
-        b_series->setBaseColor(QColor("LimeGreen"));
-        m_series->dataProxy()->addItems(m_data);
-        m_series->setName("恶性肿瘤");
-        m_series->setItemSize(0.1f);
-        m_series->setBaseColor(QColor("Crimson"));
-        q3DScatter->addSeries(b_series);
-        q3DScatter->addSeries(m_series);
-        q3DScatter->setAspectRatio(1);
-        q3DScatter->setHorizontalAspectRatio(1);
+        init_3d_scatter();
+    }else{
+
     }
+}
+
+void PCADialog::dimValueChanged(int value) {
+    go_PCA();
+}
+
+void PCADialog::init_3d_scatter() {
+    auto *q3DScatter = new Q3DScatter();
+    q3DScatter->activeTheme()->setType(Q3DTheme::ThemePrimaryColors);
+    q3DScatter->setShadowQuality(QAbstract3DGraph::ShadowQualitySoftLow);
+    q3DScatter->scene()->activeCamera()->setCameraPreset(Q3DCamera::CameraPresetFront);
+    if(scatter_3d_widget && ui->horizontalLayout->count() == 2){
+        ui->horizontalLayout->removeWidget(scatter_3d_widget);
+        scatter_3d_widget = nullptr;
+    }
+    if(scatter_2d_widget && ui->horizontalLayout->count() == 2){
+        ui->horizontalLayout->removeWidget(scatter_2d_widget);
+        scatter_2d_widget = nullptr;
+    }
+    scatter_3d_widget = QWidget::createWindowContainer(q3DScatter);
+    q3DScatter->setFlags(q3DScatter->flags() ^ Qt::FramelessWindowHint);
+    scatter_3d_widget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    ui->horizontalLayout->addWidget(scatter_3d_widget);
+    ui->horizontalLayout->setStretch(0, 1);
+    ui->horizontalLayout->setStretch(1, 3);
+    auto *b_series = new QScatter3DSeries;
+    auto *m_series = new QScatter3DSeries;
+    QScatterDataArray b_data;
+    QScatterDataArray m_data;
+    auto res = pca(points, 3);
+    float v_0_max = -1000, v_1_max = -1000, v_2_max = -1000;
+    float v_0_min = 1000, v_1_min = 1000, v_2_min = 1000;
+    for (int i = 0; i < points.size(); i++) {
+        float v_0 = res(i, 0);
+        float v_1 = res(i, 1);
+        float v_2 = res(i, 2);
+        if (point_status[i]) {
+            m_data << QVector3D(v_0, v_1, v_2);
+        } else {
+            b_data << QVector3D(v_0, v_1, v_2);
+        }
+        MAX_MIN_SET(0);
+        MAX_MIN_SET(1);
+        MAX_MIN_SET(2);
+    }
+    float v_0_offset, v_1_offset, v_2_offset;
+    OFFSET(0);
+    OFFSET(1);
+    OFFSET(2);
+    q3DScatter->axisX()->setTitle("PC1");
+    q3DScatter->axisY()->setTitle("PC2");
+    q3DScatter->axisZ()->setTitle("PC3");
+    q3DScatter->axisX()->setRange(v_0_min - v_0_offset, v_0_max + v_0_offset);
+    q3DScatter->axisY()->setRange(v_1_min - v_1_offset, v_1_max + v_1_offset);
+    q3DScatter->axisZ()->setRange(v_2_min - v_2_offset, v_2_max + v_2_offset);
+    b_series->dataProxy()->addItems(b_data);
+    b_series->setName("良性肿瘤");
+    b_series->setItemSize(0.1f);
+    b_series->setBaseColor(QColor("LimeGreen"));
+    m_series->dataProxy()->addItems(m_data);
+    m_series->setName("恶性肿瘤");
+    m_series->setItemSize(0.1f);
+    m_series->setBaseColor(QColor("Crimson"));
+    q3DScatter->addSeries(b_series);
+    q3DScatter->addSeries(m_series);
+    q3DScatter->setAspectRatio(1);
+    q3DScatter->setHorizontalAspectRatio(1);
+}
+
+void PCADialog::init_2d_scatter() {
+    if(scatter_3d_widget && ui->horizontalLayout->count() == 2){
+        ui->horizontalLayout->removeWidget(scatter_3d_widget);
+        scatter_3d_widget = nullptr;
+    }
+    if(scatter_2d_widget && ui->horizontalLayout->count() == 2){
+        ui->horizontalLayout->removeWidget(scatter_2d_widget);
+        scatter_2d_widget = nullptr;
+    }
+    scatter_2d_widget = new QChartView(this);
+    auto *chart = new QChart();
+    auto *b_series = new QScatterSeries(this);
+    b_series->setName("良性肿瘤");
+    b_series->setMarkerSize(10);
+    auto *m_series = new QScatterSeries(this);
+    m_series->setName("恶性肿瘤");
+    m_series->setMarkerSize(10);
+
+    auto res = pca(points, 2);
+    float v_0_max = -1000, v_1_max = -1000;
+    float v_0_min = 1000, v_1_min = 1000;
+    for (int i = 0; i < points.size(); i++) {
+        float v_0 = res(i, 0);
+        float v_1 = res(i, 1);
+        if (point_status[i]) {
+            m_series->append(QPointF(v_0, v_1));
+        } else {
+            b_series->append(QPointF(v_0, v_1));
+        }
+        MAX_MIN_SET(0);
+        MAX_MIN_SET(1);
+    }
+
+    auto *axisX = new QValueAxis((QObject *) this);
+   auto *axisY = new QValueAxis((QObject *) this);
+    double h_offset = (ind_max - ind_min) * 0.05;
+    axisX->setRange(ind_min - h_offset, ind_max + h_offset);
+    axisX->setTickCount(10);
+    axisX->setLabelFormat("%.2f");
+    axisX->setTitleText(name_col_ind);
+    chart->addAxis(axisX, Qt::AlignBottom);
+
+    double v_offset = (dep_max - dep_min) * 0.05;
+    axisY->setRange(dep_min - v_offset, dep_max + v_offset);
+    axisY->setTickCount(10);
+    axisY->setLabelFormat("%.2f");
+    axisY->setTitleText(name_col_dep);
+    chart->addAxis(axisY, Qt::AlignLeft);
+
+    chart->addSeries(b_series);
+    b_series->attachAxis(axisX);
+    b_series->attachAxis(axisY);
+    ui->chart->setChart(chart);
 }
